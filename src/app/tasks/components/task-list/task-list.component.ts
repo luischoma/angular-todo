ec5d1f4413719957618e5689cf8bd2d70bf1c3c1
Luis Choma <luisp.choma@gmail.com>
2025-02-09 04:27:01 -0300

    feat: Create Task List basic grid view

--- src/app/tasks/components/task-list/task-list.component.ts
+++ src/app/tasks/components/task-list/task-list.component.ts
@@ -7,11 +7,18 @@ import { Task } from 'src/app/core/models/task.model';
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss'],
 })
-export class TaskListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TaskListComponent {
+  tasks: Task[] = [
+    { id: 1, name: 'Todo 1', completed: false },
+    { id: 2, name: 'Todo 2', completed: true },
+    { id: 3, name: 'todo 3333 ', completed: true },
+    { id: 4, name: '...', completed: true },
+    { id: 5, name: 'vbbb', completed: true },
+    { id: 3, name: 'todo 3333 ', completed: true },
+    { id: 4, name: '...', completed: true },
+    { id: 5, name: 'vbbb', completed: true },
+    { id: 3, name: 'todo 3333 ', completed: true },
+    { id: 4, name: '...', completed: true },
+    { id: 5, name: 'vbbb', completed: true },
+  ];
 }

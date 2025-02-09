dfbd938a28d955766a729ef8cc2cbd4fbe7e138f
Luis Choma <luisp.choma@gmail.com>
2025-02-08 23:54:11 -0300

    chore: Add basic tasks components folder structure

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TaskListComponent, TaskItemComponent],
+  imports: [CommonModule],
+  exports: [TaskListComponent],
 })
-export class TasksModule { }
+export class TasksModule {}

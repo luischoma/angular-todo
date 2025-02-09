a4a67763621bc2ffc34e199693137f31145cc8e9
Luis Choma <luisp.choma@gmail.com>
2025-02-09 01:55:54 -0300

    chore: Migrate from pure CSS to SASS

--- src/app/tasks/components/task-item/task-item.component.ts
+++ src/app/tasks/components/task-item/task-item.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from 'src/app/core/models/task.model';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+  styleUrls: ['./task-item.component.scss'],
 })
-export class TaskItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TaskItemComponent {
+  @Input() task: Task;
 }
--- src/app/tasks/components/task-list/task-list.component.ts
+++ src/app/tasks/components/task-list/task-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Task } from 'src/app/core/models/task.model';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  styleUrls: ['./task-list.component.scss'],
 })
 export class TaskListComponent implements OnInit {
 

94d0f8b1da0d2a3ccc9c8e83bcc612c9dacaea14
Luis Choma <luisp.choma@gmail.com>
2025-02-09 21:03:54 -0300

    feat: Create Add Task button

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -10,7 +12,13 @@ import { TasksPageComponent } from './pages/tasks-page/tasks-page.component';
 
 @NgModule({
   declarations: [TaskListComponent, TaskItemComponent, TasksPageComponent],
-  imports: [CommonModule, SharedModule, MatCardModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   exports: [TasksPageComponent],
 })
 export class TasksModule {}

45b20f5960b34dce1a47c7f336c3bc2cce069edd
Luis Choma <luisp.choma@gmail.com>
2025-02-10 19:08:33 -0300

    test: Add tests for Theme Toggle component

--- src/app/shared/components/theme-toggle/theme-toggle.component.spec.ts
+++ src/app/shared/components/theme-toggle/theme-toggle.component.spec.ts
@@ -1,25 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ThemeToggleComponent } from './theme-toggle.component';
+import { ThemeService } from '../../../core/services/theme.service';
+import { By } from '@angular/platform-browser';
 
 describe('ThemeToggleComponent', () => {
   let component: ThemeToggleComponent;
   let fixture: ComponentFixture<ThemeToggleComponent>;
+  let themeServiceSpy: jasmine.SpyObj<ThemeService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ThemeToggleComponent ]
-    })
-    .compileComponents();
-  });
+    themeServiceSpy = jasmine.createSpyObj('ThemeService', ['toggleTheme']);
 
-  beforeEach(() => {
+    await TestBed.configureTestingModule({
+      declarations: [ThemeToggleComponent],
+      providers: [{ provide: ThemeService, useValue: themeServiceSpy }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ThemeToggleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call toggleTheme() on button click', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+
+    button.triggerEventHandler('click', null);
+    expect(themeServiceSpy.toggleTheme).toHaveBeenCalled();
   });
 });
